9608d4cd6f404bb93011e00df8cbd43098057b6e
nevzorovboris <nevzorovboris842@gmail.com>
2024-10-25 14:02:35 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://strongdoors.store'
+	const proxyUrl = 'https://culturedmilkfoods.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
