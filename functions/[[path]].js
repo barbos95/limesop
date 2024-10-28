493b25138d53152a7d8545ec0937a2f0b4b16a9d
victorsem-boop <victorsemenka@gmail.com>
2024-10-28 04:03:34 +0200

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://culturedmilkfoods.store'
+	const proxyUrl = 'https://barbershop0.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
