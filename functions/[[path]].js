ca748da3c59321fadba4089ba34762c634e4708f
barbos95 <valeraprohor997@gmail.com>
2024-11-09 09:53:03 +0200

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://barbershop0.store'
+	const proxyUrl = 'https://barbosshop.store'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
